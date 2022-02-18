b141f6bac976c23bdb362e964623bbe4e21c3d07
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-02-18 08:30:53 +0100

    quick fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Main } from "./componenets/Main";
-import { BrowserRouter, Router } from "react-router-dom";
 
 function App() {
   return (
     
     <div className="App">
- <BrowserRouter>
- <Router>
 
       <Main />
- </Router>
- </BrowserRouter>
       </div>
      
     
